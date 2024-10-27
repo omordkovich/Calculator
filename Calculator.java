fc0c9aa3ce7cec2de98ca6abec3be1920a73e8e4
omordkovich <o.mordkovich@hotmail.com>
2024-10-27 11:40:51 +0100

    added new code

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 10;
+        int b = 5;
+        int input = scanner.nextInt();
+        switch (input){
+
+        }
+    }
+}
